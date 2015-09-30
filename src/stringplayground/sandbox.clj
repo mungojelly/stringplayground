a66265d73844bd55817c72c206a683aa2e4ee91b
Mungojelly <mungojelly@gmail.com>
2015-09-29 21:16:53 -0400

    force-line-length for hard wrapping
    Here's a function with more general utility-- hard line wrapping.

--- src/stringplayground/sandbox.clj
+++ src/stringplayground/sandbox.clj
@@ -5,9 +5,9 @@
 (defn hello
   "Says hello to the specified world.
 
-   Usage example: 
+  Usage example: 
 
-   (hello \"cello\") => \"hi, cello\""
+  (hello \"cello\") => \"hi, cello\""
   [world]
   (str "hi, " world))
 
@@ -22,10 +22,10 @@
 
   Usage example:
 
-  (cellos->armadillos \"Oh look, there's a purple cello!\") =>
-  \"Oh look, there's a purple armadillo!\""
-   [s]
-   (clojure.string/replace s #"cello" "armadillo"))
+  (cellos->armadillos \"Oh look, there's a purple cello!\") 
+  => \"Oh look, there's a purple armadillo!\""
+  [s]
+  (clojure.string/replace s #"cello" "armadillo"))
 
 (midje/fact
   "cellos->armadillos replaces every 'cello' with 'armadillo'"
@@ -42,12 +42,54 @@
 (defn say-hi-to-an-armadillo
   "Returns an armadillo greeting.
 
-   Usage example:
+  Usage example:
 
-   (say-hi-to-an-armadillo) => \"hi, armadillo\""
+  (say-hi-to-an-armadillo) => \"hi, armadillo\""
   []
   (cellos->armadillos (hello "cello")))
 
-(midje/fact
- "say-hi-to-an-armadillo greets an armadillo, assuming that armadillo can read"
+(midje/fact "say-hi-to-an-armadillo greets an armadillo"
  (say-hi-to-an-armadillo) => "hi, armadillo")
+
+(defn remove-newlines
+  "Removes all newlines from a string.
+
+  Usage example:
+
+  (remove-newlines \"new\\nlines\") => \"newlines\""
+  [s]
+  (clojure.string/replace s #"\n" ""))
+
+(midje/fact
+ "remove-newlines removes newlines from a string"
+  (remove-newlines "\n\n\n") => ""
+  (remove-newlines "stays the same")
+                => "stays the same"
+  (remove-newlines "this is a \ntest")
+                => "this is a test"
+  (remove-newlines "removes more \nthan one\n newline")
+                => "removes more than one newline"
+  (remove-newlines "removes newline at end\n")
+                => "removes newline at end")
+
+(defn force-line-length
+  "Removes all newlines from a string and then 
+  hard wraps it to the specified line length.
+
+  (force-line-length 3 \"te\\nst\\nin\\ng\") => \"tes\\ntin\\ng\""
+  [partition-length input-string]
+  (let [unbroken (remove-newlines input-string)]
+    (clojure.string/join
+     "\n"
+     (map (partial apply str)
+          (partition-all partition-length unbroken)))))
+
+(midje/fact
+ "force-line-length hard wraps strings"
+ (force-line-length 3 "hi this is a test")
+ => "hi \nthi\ns i\ns a\n te\nst")
+
+(midje/fact
+ "force-line-length strips existing newlines"
+ (force-line-length 5 "hi\n new\nlines\n\n\n")
+ => "hi ne\nwline\ns")
